ddbca987d553bdc37fb356bda63d889b2adfacab
Shinichi Yamashita <ichi0g0y@gmail.com>
2020-05-15 07:12:51 +0900

    :memo: change avatar etc...

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -8,7 +8,10 @@ const navItems: { label: string; page?: string; link?: string }[] = [
   { label: 'Home', page: '/' },
   { label: 'Blog', page: '/blog' },
   { label: 'Contact', page: '/contact' },
-  { label: 'Source Code', link: 'https://github.com/ijjk/notion-blog' },
+  {
+    label: 'Source Code',
+    link: 'https://github.com/ichi0g0y/notion-blog-customized',
+  },
 ]
 
 const ogImageUrl = 'https://notion-blog.now.sh/og-image.png'
@@ -26,7 +29,7 @@ export default ({ titlePre = '' }) => {
         />
         <meta name="og:title" content="My Notion Blog" />
         <meta property="og:image" content={ogImageUrl} />
-        <meta name="twitter:site" content="@_ijjk" />
+        <meta name="twitter:site" content="ichi0g0y" />
         <meta name="twitter:card" content="summary_large_image" />
         <meta name="twitter:image" content={ogImageUrl} />
       </Head>
